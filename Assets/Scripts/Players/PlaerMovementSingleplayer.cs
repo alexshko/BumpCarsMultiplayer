e9de12899de1ca43d4472eb8e569a42c0fce3097
alexs <alexs@anyverse.vc>
2022-10-22 18:06:51 +0300

    .

--- Assets/Scripts/Players/PlaerMovementSingleplayer.cs
+++ Assets/Scripts/Players/PlaerMovementSingleplayer.cs
@@ -8,10 +8,16 @@ public class PlaerMovementSingleplayer : MonoBehaviour
     [SerializeField] float speedOfMove = 5;
     [SerializeField] float speedOfTurn = 100;
 
+    [SerializeField] AnimationCurve rotSpeedCurve;
+
     [Inject]
     Rigidbody _rb;
+
     [Inject]
     IPlayerInput _input;
+
+    float directionDiff;
+    float torqueToApply;
 
     //[Inject]
     //public PlaerMovementSingleplayer(Rigidbody rb, IPlayerInput input)
@@ -29,11 +35,20 @@ public class PlaerMovementSingleplayer : MonoBehaviour
             return;
         }
 
-        float directionDiff = Vector3.Dot(transform.forward, _input.InputDirection.normalized);
-        if (directionDiff < 0.95f)
-        {            
+        directionDiff = Vector3.Dot(transform.forward, _input.InputDirection.normalized);
+        Debug.LogFormat("DirectionDiff value: {0}", directionDiff);
+        if (directionDiff < 0.99995f)
+        {
+            float signAngle = Mathf.Sign(Vector3.SignedAngle(transform.forward, _input.InputDirection.normalized, Vector3.up));
             //_rb.AddTorque(Vector3.up * Mathf.Sign(Vector3.SignedAngle(transform.forward, _input.InputDirection.normalized * speedofTurn, Vector3.up))  * speedofTurn, ForceMode.Force);
-            _rb.AddTorque(Vector3.up *  (1 - directionDiff) * speedOfTurn, ForceMode.Force);
+            torqueToApply = signAngle * rotSpeedCurve.Evaluate(directionDiff) * speedOfTurn;
+            _rb.AddTorque(Vector3.up * torqueToApply, ForceMode.Force);
         }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 20), directionDiff.ToString());
+        GUI.Box(new Rect(0, 30, 100, 20), torqueToApply.ToString());
     }
 }
